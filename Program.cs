32967be32cef18172ac9eec5afcb237ed2356d57
Алексей Валерьевич Белоусов <chiisai@yandex.ru>
2021-03-09 22:13:23 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SF3._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
