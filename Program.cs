be268ca037761768be6edfd2e39a029cb652fcc3
Алексей Валерьевич Белоусов <chiisai@yandex.ru>
2021-03-09 23:12:16 +0300

    Финальная практика модуля «Анкетируем пользователей»

--- Program.cs
+++ Program.cs
@@ -7,15 +7,19 @@ namespace SF3._7
         static void Main(string[] args)
         {
             Console.WriteLine("Enter your name!");
-            string name = Console.ReadLine();
+            var name = Console.ReadLine();
             Console.WriteLine("Enter your age!");
-            var age = int.Parse(Console.ReadLine());
+            var age = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Your name is {0}!! But your age is {1}!!!", name, age);
 
-            Console.WriteLine("Сообщи день недели!");
-            var myday = (DaysOfWeek) int.Parse(Console.ReadLine());
-            Console.WriteLine("День недели - {0}", myday);
+            //Console.WriteLine("Сообщи день недели!");
+            //var myday = (DaysOfWeek) int.Parse(Console.ReadLine());
+            //Console.WriteLine("День недели - {0}", myday);
+
+            Console.WriteLine("Сообщи дату рождения!");
+            var birthdate = Console.ReadLine();
+            Console.WriteLine("День рождения - {0}, якобы.", birthdate);
 
             Console.ReadKey();
         }
